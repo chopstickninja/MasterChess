6b100553071d0755847e14bab99ba458cc6d1cb6
App Academy Student <student@appacademy.io>
2013-07-23 14:45:37 -0700

    Fleshing out board class

--- chess.rb
+++ chess.rb
@@ -5,91 +5,58 @@ class Player
 end
 
 class Board
-  # 8 x 8
+  def build_board
+    @grid = []
+    8.times.do { @grid << [] }
+  end
+
+  def build_set_of_pieces
+    pieces = Hash.new { |hash, key| hash[key] = []}
+
+    # Build me some white pawns
+    8.times do |x|
+      pieces[white] << Pawn.new([x,1], "white", @board)
+    end
+
+    #
+
+
+  end
+
+  def initialize
+    self.build_board
+  end
+
+  # Setter
+  def []=(x, y)
+    @grid[x][y]
+  end
+
+  #Getter
+  def [](x, y)
+    @grid[x][y]
+  end
+
 end
 
 
 class Piece
   attr_accessor :position
 
-  def initialize(position)
+  def initialize(position, color, board)
     @position = position
+    @color    = color
+    @board    = board
   end
 
   def move(new_position)
     possible_positions = self.possible_positions
-    raise PositionError unless possible_positions.include?(new_position)
+    raise PositionError unless valid_move?(new_position)
     @position = new_position
   end
 
   def valid_move?(new_position)
     self.possible_positions.include?(new_position)
-  end
-end
-
-class Stepper < Piece
-
-  def initialize(position, steps)
-    super(position)
-    @steps = steps
-  end
-
-  def possible_positions
-    new_positions = []
-
-    @steps.each do |move|
-      i, j = move
-      x, y = @position
-      new_positions << [x + i, y + j]
-    end
-    new_positions.select {|x, y| (x >= 0 && x < 8) && (y >= 0 && y < 8)}
-  end
-end
-
-
-class Slider < Piece
-  #CANNOT LEAP OVER PIECES
-  def initialize(position, piece)
-    super(position)
-    #@steps = self.get_directions(piece_direction)
-  end
-
-  def get_directions(piece)
-    steps = []
-
-    if piece == "rook"
-      steps += self.rook_positions
-    elsif piece == "bishop"
-      # TODO
-    end
-      # TODO
-  end
-
-  def rook_positions
-    step_array = [[1, 0], [0, 1], [-1, 0], [0, -1]
-    self.calculate_positions(step_array)
-  end
-
-  def bishop_positions
-  end
-
-  def calculate_positions(step_array)
-    x, y = @position
-    new_positions = []
-
-    step_array.each do |direction|
-        until self.out_of_bounds?(x, y)
-          i, j  = direction
-          x, y  = x + i, y + j
-          new_positions << [[x, y]] unless self.out_of_bounds?(x, y)
-        end
-
-        # DUH! We need to reset back to the original spot!
-        x, y = @position
-    end
-
-    new_positions
-  end
   end
 
   def out_of_bounds?(x, y)
@@ -98,31 +65,73 @@ class Slider < Piece
 
 end
 
+class Stepper < Piece
+  def possible_positions
+    new_positions = []
+
+    self.steps.each do |move|
+      i, j = move
+      x, y = @position
+      new_positions << [x + i, y + j]
+    end
+    new_positions.select {|x, y| !self.out_of_bounds?(x, y)}
+  end
+end
+
+
+class Slider < Piece
+  def possible_positions
+    x, y = @position
+    new_positions = []
+
+    self.steps.each do |direction|
+        until self.out_of_bounds?(x, y)
+          i, j  = direction
+          x, y  = x + i, y + j
+          new_positions << [x, y] unless self.out_of_bounds?(x, y)
+        end
+
+        # DUH! We need to reset back to the original spot!
+        x, y = @position
+    end
+
+    new_positions
+  end
+end
+
+
 class King < Stepper
-  def initialize(position)
-    steps = [[0,1],[0,-1],[1,0],[-1,0],[-1,-1],[-1,1],[1,-1],[1,1]]
-    super(position, steps)
+  def steps
+    [[0, 1], [0, -1], [1, 0], [-1, 0], [-1, -1], [-1, 1], [1, -1], [1, 1]]
   end
 end
 
 
 class Knight < Stepper
-  def initialize(position)
-    steps = [[1,2],[-2,1],[2,1],[-1,2],[-2,-1],[-1,-2],[1,-2],[2,-1]]
-    super(position, steps)
+  def steps
+    [[1, 2], [-2, 1], [2, 1],[-1, 2], [-2, -1], [-1, -2], [1, -2], [2, -1]]
   end
 end
 
 
-class Bishop < Piece
+class Bishop < Slider
+  def steps
+    [[1, 1], [-1, 1], [1, -1], [-1, -1]]
+  end
 end
 
 
-class Queen < Piece
+class Queen < Slider
+  def steps
+    [[1, 0], [0, 1], [-1, 0], [0, -1], [1, 1], [-1, 1], [1, -1], [-1, -1]]
+  end
 end
 
 
-class Rook < Piece
+class Rook < Slider
+    def steps
+    [[1, 0], [0, 1], [-1, 0], [0, -1]]
+  end
 end
 
 
@@ -137,5 +146,15 @@ end
 #p knight.possible_positions
 #p king.possible_positions
 
-s = Slider.new([4, 4], "rook")
-p s.rook_positions
+r = Rook.new([3,4])
+b = Bishop.new([3,4])
+q = Queen.new([3,4])
+kg = King.new([3,4])
+
+#p r.possible_positions
+#p b.possible_positions
+#p q.possible_positions
+#p s.rook_positions
+#p s.bishop_positions
+
+p kg.possible_positions
