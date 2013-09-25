b50f41e7f1d8977231ab476aae2cc5e3c76bc563
chopstickninja <j3h15u@users.noreply.github.com>
2013-09-24 18:10:35 -0700

    Update chess.rb

--- chess.rb
+++ chess.rb
@@ -1,8 +1,6 @@
 require 'colorize'
 require 'debugger'
 
-# My Filesrequire "pieces"
-
 require './board.rb'
 require './pieces.rb'
 require './player'
@@ -15,7 +13,7 @@ class ChessGame
   end
 
   def get_move
-    puts "Make a move! E.g. F2, F3" #[f,2][f,3]
+    puts "Make a move! E.g. F2, F3"
     player_moves = gets.chomp.downcase.split(",")
     move = []
 
@@ -28,9 +26,9 @@ class ChessGame
   def initialize(custom_positions = nil)
 
     if custom_positions
-      @board = Board.new(custom_positions)# unless custom_positions.nil?
+      @board = Board.new(custom_positions)
     else
-      @board = Board.new #if custom_positions.nil?
+      @board = Board.new
     end
   end
 
@@ -38,19 +36,15 @@ class ChessGame
     puts "You're playing chess..."
     until @board.won?
       play_turn
-
-      #p "Has the board been won? #{@board.won?}"
     end
   end
 
   def change_player
     @current_player = @player2 if (@current_player == @player1)
     @current_player = @player1
-    #FIX THIS SHIT
   end
 
   def play_turn
-    #make sure color matches player whose turn it is
     puts @board
     move = get_move
     @board.move(move[0], move[1])
@@ -60,20 +54,3 @@ end
 
 chess = ChessGame.new
 chess.play
-
-
-# p chess.board[[0, 1]].class
-# p chess.board[[0, 1]].possible_positions
-# puts
-#
-# p chess.board[[1, 0]].class
-# p chess.board[[1, 0]].possible_positions
-#
-# p chess.board[[2, 0]].class
-# p chess.board[[2, 0]].possible_positions
-#
-# p chess.board[[3, 0]].class
-# p chess.board[[3, 0]].possible_positions
-#
-# p chess.board[[4, 0]].class
-# p chess.board[[4, 0]].possible_positions
